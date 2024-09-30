3d001402650bae1d52ee67e2b0cd4f52a2d8906f
篠原 <shinohara@shinoharanoMacBook-Air.local>
2024-09-30 14:31:09 +0900

    templates,view追加

--- KidDic/KidDic/urls.py
+++ KidDic/KidDic/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from app.views import PortfolioView
+from app.views import PortfolioView, SignupView, LoginView, HomeView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('',PortfolioView.as_view(), name="portfolio")
+    path('',PortfolioView.as_view(), name="portfolio"),
+    path('signup/',SignupView.as_view(), name="signup"),
+    path('login/',LoginView.as_view(), name="login"),
+    path('home/',HomeView.as_view(), name="home"),
 ]
--- KidDic/app/views.py
+++ KidDic/app/views.py
@@ -5,4 +5,16 @@ from django.views import View
 class PortfolioView(View):
     def get(self, request):
         return render(request, "portfolio.html")
+
+class SignupView(View):
+    def get(self, request):
+        return render(request, "signup.html")
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, "login.html")
+
+class HomeView(View):
+    def get(self, request):
+        return render(request, "home.html")
     
