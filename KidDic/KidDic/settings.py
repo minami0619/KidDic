e6a8586aa7a2301a6d6ee32384b82ec2da85b677
篠原 <shinohara@shinoharanoMacBook-Air.local>
2024-10-01 11:06:20 +0900

    新規登録画面のtemplates修正

--- KidDic/KidDic/settings.py
+++ KidDic/KidDic/settings.py
@@ -81,7 +81,10 @@ DATABASES = {
     }
 }
 
-
+AUTH_USER_MODEL = "app.User"
+# AUTHENTICATION_BACKENDS = [
+#     "app.backends.UserAuthBackend"
+# ]
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
